dd1689317c9da475257248773fb1d2849cb7a7f9
Виолетта Чевтайкина <vilka.14p@mail.ru>
2023-05-15 00:16:12 +0500

    +

--- 25/25/MainWindow.xaml.cs
+++ 25/25/MainWindow.xaml.cs
@@ -17,9 +17,79 @@ namespace WpfApp_урааааааааааа_я_тебя_нашла
 {
     public partial class MainWindow : Window
     {
+        int num1;
+        int num2;
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void press1Button_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "1";
+        }
+        private void press2Button_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "2";
+        }
+        private void press3Button_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "3";
+        }
+        private void press4Button_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "4";
+        }
+        private void press5Button_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "5";
+        }
+        private void press6Button_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "6";
+        }
+        private void press7Button_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "7";
+        }
+        private void press8Button_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "8";
+        }
+        private void press9Button_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "9";
+        }
+        private void plusButton_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "+";
+            
+        }
+        private void minusButton_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "-";
+        }
+        private void umnButton_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "*";
+        }
+        private void delButton_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "/";
+        }
+        private void resultButton_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text = Calculation(inputTextBlock.Text);
+        }
+        private void zeroButton_Click(object sender, RoutedEventArgs e)
+        {
+            inputTextBlock.Text += "0";
+        }
+        private string Calculation(string expression)
+        {
+            DataTable dt = new DataTable();
+            object result = dt.Compute(expression, "");
+            return Convert.ToString(result);
+        }
+    }   
     }
 }
