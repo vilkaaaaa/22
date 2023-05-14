8da42909e42311e2756d6ffee08dc770fc69fdc8
Виолетта Чевтайкина <vilka.14p@mail.ru>
2023-05-15 00:45:28 +0500

    pz

---
+++ 26/pz_26/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pz_26
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool ital = false;
+        bool bld = false;
+        bool und = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (bld)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            if (ital)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            if (und)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+
+            textBox.FontWeight = FontWeights.Bold;
+        }
+        private void italicc(object sender, RoutedEventArgs e)
+        {
+            if (!ital)
+                ital = true;
+            else
+                ital = false;
+        }
+        private void blodd(object sender, RoutedEventArgs e)
+        {
+            if (!bld)
+                bld = true;
+            else
+                bld = false;
+        }
+        private void underr(object sender, RoutedEventArgs e)
+        {
+            if (!und)
+                und = true;
+            else
+                und = false;
+        }
+    }
+}
