551c1d0cde954314fdb179c7257f917348fec4cc
Виолетта Чевтайкина <vilka.14p@mail.ru>
2023-05-15 00:06:22 +0500

    +

---
+++ 25/25/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp_урааааааааааа_я_тебя_нашла
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
