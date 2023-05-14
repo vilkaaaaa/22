b85738638142676871abbd23fa766cd8d0fdfe42
Виолетта Чевтайкина <vilka.14p@mail.ru>
2023-05-14 20:22:36 +0500

    21

---
+++ 21/21/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+
+namespace _22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime otprav = new DateTime(2023, 2, 20, 12, 30, 00);
+            Ticket sasha = new Ticket(2654, 5649, "льгот", 7, 11, otprav);
+            sasha.PrintInfo();
+
+
+        }
+    }
+}
---
+++ 21/21/ticket.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22
+{
+    
+    class Ticket
+    {
+        public int id { get; set; }
+        public decimal ownerPassport { get; set; }
+        public string type { get; set; }
+        public int trainNumber { get; set; }
+        public int place { get; set; }
+        public DateTime departureTime { get; set; }
+        static double hours { get; set; }
+        public Ticket(int id, decimal ownerPassport, string type, int trainNumber, int place, DateTime departureTime)
+        {
+            this.id = id;
+            this.ownerPassport = ownerPassport;
+            this.type = type;
+            this.trainNumber = trainNumber;
+            this.place = place;
+            this.departureTime = departureTime;
+        }
+        public void GetTimeLeft() 
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan ostalos = departureTime - now;
+            hours = ostalos.TotalHours;
+
+        }
+
+        public void PrintInfo()
+        {
+            GetTimeLeft();
+            Console.Write("айди ");
+            Console.WriteLine(id);
+            Console.Write("паспорт ");
+            Console.WriteLine(ownerPassport);
+            Console.Write("тип билета ");
+            Console.WriteLine(type);
+            Console.Write("номер поезда ");
+            Console.WriteLine(trainNumber);
+            Console.Write("номер места ");
+            Console.WriteLine(place);
+            Console.Write("часов до отправки ");
+            Console.WriteLine(Math.Round(hours));
+            Console.Write("отправка в ");
+            Console.WriteLine(departureTime);
+
+        }
+    }
+    
+}
