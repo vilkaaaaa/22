a396859d41d1f1a40fb95f6c61615a531cb8ac6b
Виолетта Чевтайкина <vilka.14p@mail.ru>
2023-05-14 20:29:09 +0500

    20 pz

---
+++ 20/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace PZ_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "source.txt";
+            string parameterName = "myParameter";
+            string replacement = "newParameterValue";
+
+            try
+            {
+               
+                string fileContent = File.ReadAllText(filePath);
+
+                int startIndex = fileContent.IndexOf(parameterName);
+
+                if (startIndex != -1)
+                {
+                    int endIndex = startIndex + parameterName.Length;
+                    fileContent = fileContent.Substring(0, startIndex) + replacement + fileContent.Substring(endIndex);
+
+                    File.WriteAllText(filePath, fileContent);
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Файл не найден: " + ex.Message);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Ошибка ввода/вывода: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+    }
+}
