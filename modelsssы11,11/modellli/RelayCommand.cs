b08a87eda4fd2909c7379d5576fa994346ea55c8
chevtaykina.vv1700 <lytikr@mail.ru>
2024-11-12 16:41:36 +0300

    12.11 new new

--- modelsssы11,11/modellli/RelayCommand.cs
+++ modelsssы11,11/modellli/RelayCommand.cs
@@ -41,4 +41,39 @@ namespace modellli
             }
         }
     }
+    internal class RelayCommand <T>: ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private Action<T> _execute;
+        private Func<T,bool> _canExecute;
+        public RelayCommand(Action<T> execute) => _execute = execute;
+        public RelayCommand(Action<T> execute, Func<T,bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+        public bool CanExecute(object? parameter)
+        {
+            if (_canExecute != null)
+            {
+                T param = (T)parameter;
+                return _canExecute(param);
+            }
+            if (_execute != null)
+                return true;
+            return false;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (_execute != null)
+            {
+                _execute((T)parameter);
+            }
+        }
+    }
 }
